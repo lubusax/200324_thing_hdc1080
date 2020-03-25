f48bed0a52bab671847957bddd3a751ff8896cea
lubusax <lu.bu.sax@gmail.com>
2020-03-25 06:53:28 +0100

    initial

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+'name': "Thing HDC1080",
+'summary': "Driver for Temperature and Humidity Sensor ti HDC1080",       
+'description': """Driver for Temperature and
+    relative Humidity Sensor Texas Instruments
+    HDC1080""",
+'author': "Lu Bu",
+'website': "http://www.thingsintouch.com",
+'category': 'Uncategorized',
+'version': '12.0.1',
+'depends': ['base'],
+'data': [
+    #'security/groups.xml',
+    'security/ir.model.access.csv',
+    #'views/things_menus.xml',
+    #'views/things_gate.xml',
+    'views/thing_HDC1080.xml',
+
+    ],
+# 'demo': ['demo.xml'],
+}
