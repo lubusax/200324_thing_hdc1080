af8445169f9a8358ddacc432e99911ea3904f9b1
lubusax <lu.bu.sax@google.com>
2020-04-03 19:04:10 +0200

    thing type created

--- controllers/main.py
+++ controllers/main.py
@@ -6,7 +6,11 @@ class ThingsGateExtended(ThingsGate):
     @http.route()    
     def messageFromGate(self, routeFrom, **kwargs):
         response = super().messageFromGate(routeFrom, **kwargs)
-        print('FROM HDC1080 - gateSending is ', response)
-        print (' this is from HDC1080 - something came from', routeFrom)
-        return {'message': 'thank you from HDC1080'}
+        thingType= kwargs.get('thing type')
+        print ('thingType from HDC1080 ', thingType)
+        if thingType== 'HDC1080':
+            print('FROM HDC1080 - gateSendingDict is ', response)
+            print (' this is from HDC1080 - something came from', routeFrom)
+            response['thing type created'] = 'HDC1080'
+        return response
 
