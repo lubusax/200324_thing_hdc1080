6de55759af31fe0d51d517923c068457cff33dd6
lubusax <lu.bu.sax@google.com>
2020-04-03 20:04:49 +0200

    routes to create new things

--- controllers/main.py
+++ controllers/main.py
@@ -6,11 +6,13 @@ class ThingsGateExtended(ThingsGate):
     @http.route()    
     def messageFromGate(self, routeFrom, **kwargs):
         response = super().messageFromGate(routeFrom, **kwargs)
-        thingType= kwargs.get('thing type')
-        print ('thingType from HDC1080 ', thingType)
-        if thingType== 'HDC1080':
-            print('FROM HDC1080 - gateSendingDict is ', response)
-            print (' this is from HDC1080 - something came from', routeFrom)
-            response['thing type created'] = 'HDC1080'
+        print('response HDC1080 ', response)
+        routeKnown = response['route known']
+        if routeKnown == 'true':
+            thingType= kwargs.get('thing type')
+            if thingType== 'HDC1080':
+                print('FROM HDC1080 - gateSendingDict is ', response)
+                print (' this is from HDC1080 - something came from', routeFrom)
+                response['thing type created'] = 'HDC1080'
         return response
 
