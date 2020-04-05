cd15274b72aa73c3370fa883214d103e9f06fd69
lubusax <lu.bu.sax@google.com>
2020-04-05 19:30:12 +0200

    autosave - response for new thing connection

--- controllers/main.py
+++ controllers/main.py
@@ -5,21 +5,33 @@ class ThingsGateExtended(ThingsGate):
 
     @http.route()    
     def messageFromGate(self, routeFrom, **kwargs):
-        moduleType = 'HDC1080' # only this Variable should be modified
+        moduleType = 'HDC1080' # this Variable should be modified
+        ThingModel = http.request.env['things.hdc1080']
+                # this Model should be modified
 
         response = super().messageFromGate(routeFrom, **kwargs)
 
-        response = self.modifyResponse(response, moduleType, routeFrom, **kwargs)
+        response = self.modifyResponse(response,
+                ThingModel, moduleType, routeFrom, **kwargs)
 
         return response
 
-    def modifyResponse(self, response, moduleType, routeFrom, **kwargs):
+    def modifyResponse(self, response,
+                ThingModel, moduleType, routeFrom, **kwargs):
 
-        routeKnown = response['route known']
+        routeKnown = response['gate route known']
         thingType= kwargs.get('thing type')
+        thingIdentifier = kwargs.get('thing identifier')
 
-        if routeKnown == 'true' and thingType== moduleType:
-            thingIdentifier = kwargs.get('thing identifier') or 'none'
+                   
+        if not thingIdentifier:
+            response['error']   = 'Name must exist'            
+        elif ThingModel.sudo().search(
+                [('name', '=', thingIdentifier)]):
+            response['error']   = 'Name must be unique'
+        elif  routeKnown  == 'false':
+            response['error']   = 'Given Gate Route unknown'
+        elif thingType   == moduleType:
 
             new_thing = {
                 'name' : thingIdentifier,
@@ -31,12 +43,19 @@ class ThingsGateExtended(ThingsGate):
             gateSending.write({
                 idsField : [(0,0,new_thing)]
                 })
-            
-            response['thing type created'] = moduleType
 
+            thingCreated = ThingModel.sudo().search(
+                [('name', '=', thingIdentifier)]).sudo().read()[0]
+
+            response['route from']          = thingCreated['route_from']
+            response['route to']            = thingCreated['route_to']           
+            response['thing type created']  = moduleType
+            response['error']               = 'none'
+
+            #print('thing created :', thingCreated)
             # print('FROM ', moduleType)
             # print('\nGate Created Dict is ', gateSending.sudo().read()[0])
-            # print('\nResponse is ', response)
+            print('\nResponse is ', response)
             # print ('\nSomething came from', routeFrom)
 
         return response
